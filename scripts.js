1f92d846e45217f80370f7cf87878168335217af
Samuuca1 <samuelchaves_2012@hotmail.com>
2025-07-23 17:57:49 -0400

    Added Focus and Blur to a form, plus CSS

--- scripts.js
+++ scripts.js
@@ -25,9 +25,30 @@ document.addEventListener("keyup", event => {
 
 let who;
 
-document.getElementById("submit").onclick = function(event) {
+document.getElementById("submit").onclick = function (event) {
     event.preventDefault();
     who = document.getElementById("who").value;
     document.getElementById("h1Who").textContent = `Welcome ${who}, this is my lab7 Events project`
     alert(`${who} is here!`);
 }
+
+const ageInput = document.getElementById('age');
+    ageInput.addEventListener('focus', (event) => {
+        event.target.style.backgroundColor = 'lightyellow';
+        console.log('Button received focus!');
+    });
+
+function wrongAge() {
+    var age = document.getElementById('age');
+    var ageInput = parseInt(age.value, 10);
+
+    if (ageInput >= 18) {
+        age.style.backgroundColor = 'lightgreen';
+
+
+    } else {
+        age.style.backgroundColor = 'tomato';
+
+    }
+}
+
